93f083d88114704898a1cb5a823361a4ab48077f
Nirmal Singh <nirmalsingh100754@hotmail.com>
2025-01-31 00:21:15 +0530

    improved css

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,15 @@
 export default function Tasks({ tasks, markTaskAsDone, deleteTask }) {
   return (
     <div>
-      <h2 style={{color:"whitesmoke"}}>Task List</h2>
+      <h2
+        style={{
+          color: "whitesmoke",
+          position: "relative",
+          paddingRight: "15px",
+        }}
+      >
+        Task List
+      </h2>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
@@ -10,7 +18,12 @@ export default function Tasks({ tasks, markTaskAsDone, deleteTask }) {
               checked={task.done}
               onChange={() => markTaskAsDone(index)}
             />
-            <span style={{ textDecoration: task.done ? 'line-through' : 'none' , color:"wheat"}}>
+            <span
+              style={{
+                textDecoration: task.done ? "line-through" : "none",
+                color: "wheat",
+              }}
+            >
               {task.text}
             </span>
             <button onClick={() => deleteTask(index)}>Delete</button>
